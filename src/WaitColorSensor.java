e5b827543e86dce450f9c37c4389a6c93e86d36d
PaoloGuebeli <37575527+PaoloGuebeli@users.noreply.github.com>
2019-01-11 13:29:50 +0100

    Analog sensors update
    add WaitSoundSensor waits a sound level
    updated WaitColorSensor now it contains all the analog sensor range class 'cause it's the onl one

--- src/WaitColorSensor.java
+++ src/WaitColorSensor.java
@@ -2,5 +2,37 @@ import lejos.nxt.*;
 
 public class WaitColorSensor extends WaitSensor{
 	
+	private ColorLightSensor cs;
 	
+	private byte red = 0;
+	
+	private byte blue = 0;
+	
+	private byte green = 0;
+	
+	public WaitColorSensor(ColorLightSensor cs){
+		this.cs = cs;
+	}
+	
+	public void wait(){
+		while(isFinished()){
+			
+		}
+	}
+	
+	public boolean isFinished(){
+		boolean finished = false;
+		if(cs.getRed() > red-10 && cs.getRed() < red+10){
+			if(cs.getGreen() > green-10 && cs.getGreen() < green+10){
+				if(cs.getBlue() > blue-10 && cs.getBlue() < blue+10){
+					finished = true;
+				}
+			}	
+		}
+		return finished;
+	}
+	
+	public void beginWait(){
+		wait();
+	}
 }
