1efaee34ab987f08973215b5f3bfc3a3f0807d0c
PaoloGuebeli <37575527+PaoloGuebeli@users.noreply.github.com>
2019-01-11 14:04:38 +0100

    Update WaitUltrasonicSensor.java
    Add main code to WaitultrasonicSensor copied from WaitSoundSensor

--- src/WaitUltrasonicSensor.java
+++ src/WaitUltrasonicSensor.java
@@ -1,1 +1,34 @@
+import lejos.nxt.*;
+
+public class WaitUltrasonicSensor extends WaitAnalogSensor{
+	
+	private UltrasonicSensor sonic;
+	
+	public WaitUltrasonicSensor(UltrasonicSensor sonic){
+		this.sonic = sonic;
+	}
+	
+	public void wait(){
+		while(isFinished()){
+			
+		}
+	}
+	
+	public boolean isFinished(){
+		boolean finished = false;
+		if(bigger){
+			if(sonic.getDistance() > value){
+				finished = true;
 			}
+		}else{
+			if(sonic.getDistance() < value){
+				finished = true;
+			}
+		}
+		return finished;
+	}
+	
+	public void beginWait(){
+		wait();
+	}
+}
