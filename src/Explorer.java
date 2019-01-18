8b61629bfcd4d8f67d8669832de75bfd5f12dfa4
PaoloGuebeli <37575527+PaoloGuebeli@users.noreply.github.com>
2019-01-18 14:37:52 +0100

    Update Explorer.java
    Added all sensors
    -UltrasonicSensor
    -LightSensor
    -2x TouchSensor
    Created if to change direction if the bot touches something or is at 30 cm from a wall. the bot stops if he sees a black line

--- src/Explorer.java
+++ src/Explorer.java
@@ -4,6 +4,14 @@ public class Explorer{
 	private final char MOTOR_LEFT_PORT = 'A';
 	private final char MOTOR_RIGHT_PORT = 'B';
 	private Navigation navigator;
+	private UltrasonicSensor us = new UltrasonicSensor(SensorPort.S3);
+	private WaitUltrasonicSensor wus = new WaitUltrasonicSensor(us);
+	private TouchSensor tsl = new TouchSensor(SensorPort.S2);
+	private TouchSensor tsr= new TouchSensor(SensorPort.S4);
+	private WaitTouchSensor wtsl = new WaitTouchSensor(tsl);
+	private WaitTouchSensor wtsr = new WaitTouchSensor(tsr);
+	private LightSensor ls = new LightSensor(SensorPort.S1);
+	private WaitLightSensor wls = new WaitLightSensor(ls);
 
 	public Explorer(){
 		navigator = new Navigation(MOTOR_LEFT_PORT, MOTOR_RIGHT_PORT);
@@ -14,6 +22,18 @@ public class Explorer{
 	public static void main(String[] args){
 		while(true){
 			navigator.move();
+			if(wus.isFinished(false, 30)){
+				//Inserire cosa deve fare se è più vicino di 30 cm
+			}
+			if(wtsl.isFinished(0)){
+				//gira a sinistra indietro
+			}
+			if(wtsr.isFinished(0)){
+				//gira a destra indietro
+			}
+			if(wls.isFinished(false, 50)){
+				//Si ferma 
+			}
 		}
 	}
 }
