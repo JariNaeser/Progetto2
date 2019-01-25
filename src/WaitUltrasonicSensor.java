1287b464d0bce8b27fb9314436f9c95fd4410912
Jari Näser <jari.naeser@samtrevano.ch>
2019-01-25 15:54:50 +0100

    Update WaitUltrasonicSensor.java

--- src/WaitUltrasonicSensor.java
+++ src/WaitUltrasonicSensor.java
@@ -15,16 +15,15 @@ public class WaitUltrasonicSensor{
 	}
 	
 	public boolean isFinished(boolean bigger, int value){
-		boolean finished = false;
 		if(bigger){
 			if(sonic.getDistance() > value){
-				finished = true;
+				return true;
 			}
 		}else{
 			if(sonic.getDistance() < value){
-				finished = true;
+				return true;
 			}
 		}
-		return finished;
+		return false;
 	}
 }
